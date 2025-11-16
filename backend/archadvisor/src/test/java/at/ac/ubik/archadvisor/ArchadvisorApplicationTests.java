776c8bfb4b1986c5c9880085d2ec501860b4ae42
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2025-11-16 11:26:30 +0100

    Initial test setup for java spring

---
+++ backend/archadvisor/src/test/java/at/ac/ubik/archadvisor/ArchadvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.ubik.archadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchadvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
