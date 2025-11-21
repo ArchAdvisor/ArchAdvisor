3e3e1ccf2caaf2bc018ab891d774710945571bae
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2025-11-21 20:28:48 +0100

    Setting up H2 db for testing

--- backend/archadvisor/src/test/java/at/ac/ubik/archadvisor/ArchadvisorApplicationTests.java
+++ backend/archadvisor/src/test/java/at/ac/ubik/archadvisor/ArchadvisorApplicationTests.java
@@ -2,12 +2,15 @@ package at.ac.ubik.archadvisor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ArchadvisorApplicationTests {
 
     @Test
-    void contextLoads() {
+    void smokeTest() {
+        assert (1 + 1 == 2);
     }
 
 }
