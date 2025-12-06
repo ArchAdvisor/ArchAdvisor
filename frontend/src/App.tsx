af5dab1aa36ca341683fb230ec6dc39dc11731a8
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2025-12-06 18:44:44 +0100

    Add results page to path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import QuestionnaireForm from './QuestionnaireForm';
+import { Routes, Route } from "react-router-dom";
+import ResultsPage from './ResultsPage';
 
 function App() {
-  return <QuestionnaireForm />;
+  return (
+    <Routes>
+      <Route path="/" element={<QuestionnaireForm />} />
+      <Route path="/results" element={<ResultsPage />} />
+    </Routes>
+  );
 }
 
 export default App;
