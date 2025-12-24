3cc223e186da125736ab79b40fa534cac329f0b9
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2025-12-24 10:10:17 +0100

    Use Appshell for App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,18 @@ import QuestionnaireForm from './QuestionnaireForm';
 import { Routes, Route } from "react-router-dom";
 import ResultsPage from './ResultsPage';
 import FinalStackPage from './FinalPage';
+import { AppShell } from "./AppShell";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<QuestionnaireForm />} />
-      <Route path="/results" element={<ResultsPage />} />
-      <Route path="/final" element={<FinalStackPage />} />
-      <Route path="/draft/:draftId" element={<QuestionnaireForm />} />
-    </Routes>
+    <AppShell>
+      <Routes>
+        <Route path="/" element={<QuestionnaireForm />} />
+        <Route path="/results" element={<ResultsPage />} />
+        <Route path="/final" element={<FinalStackPage />} />
+        <Route path="/draft/:draftId" element={<QuestionnaireForm />} />
+      </Routes>
+    </AppShell>
   );
 }
 
