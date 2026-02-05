480de3392c811e86471a3db9465c4a6f17204fd3
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-02-05 10:40:42 +0100

    Add methods for finding specific draft and get latest to draft repo

--- backend/archadvisor/src/main/java/at/ac/ubik/archadvisor/infrastructure/persistence/repository/QuestionnaireDraftRepository.java
+++ backend/archadvisor/src/main/java/at/ac/ubik/archadvisor/infrastructure/persistence/repository/QuestionnaireDraftRepository.java
@@ -3,7 +3,14 @@ package at.ac.ubik.archadvisor.infrastructure.persistence.repository;
 import at.ac.ubik.archadvisor.infrastructure.persistence.entity.QuestionnaireDraftEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface QuestionnaireDraftRepository extends JpaRepository<QuestionnaireDraftEntity, UUID> {
+    Optional<QuestionnaireDraftEntity> findFirstByKeyDraftIdOrderByKeyVersionDesc(UUID draftId);
+
+    Optional<QuestionnaireDraftEntity> findByKeyDraftIdAndKeyVersion(UUID draftId, long version);
+
+    List<QuestionnaireDraftEntity> findByKeyDraftIdOrderByKeyVersionDesc(UUID draftId);
 }
