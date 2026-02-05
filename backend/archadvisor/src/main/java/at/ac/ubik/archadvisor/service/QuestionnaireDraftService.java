a44785afdd4680496ffb5d7e55067e3f468fb60a
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-02-05 10:41:56 +0100

    Update logic for creating new draft in service and creating a new version, when updating

--- backend/archadvisor/src/main/java/at/ac/ubik/archadvisor/service/QuestionnaireDraftService.java
+++ backend/archadvisor/src/main/java/at/ac/ubik/archadvisor/service/QuestionnaireDraftService.java
@@ -2,9 +2,11 @@ package at.ac.ubik.archadvisor.service;
 
 import at.ac.ubik.archadvisor.DTO.QuestionnaireRequestDto;
 import at.ac.ubik.archadvisor.infrastructure.persistence.entity.QuestionnaireDraftEntity;
+import at.ac.ubik.archadvisor.infrastructure.persistence.repository.QuestionnaireDraftHeadRepository;
 import at.ac.ubik.archadvisor.infrastructure.persistence.repository.QuestionnaireDraftRepository;
 import at.ac.ubik.archadvisor.mapper.QuestionnaireDraftMapper;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,31 +16,51 @@ public class QuestionnaireDraftService {
     private final QuestionnaireDraftMapper mapper;
 
     private final QuestionnaireDraftRepository questionnaireDraftRepository;
+    private final QuestionnaireDraftHeadRepository questionnaireDraftHeadRepository;
 
-    public QuestionnaireDraftService(QuestionnaireDraftMapper mapper, QuestionnaireDraftRepository questionnaireDraftRepository) {
+    public QuestionnaireDraftService(QuestionnaireDraftMapper mapper, QuestionnaireDraftRepository questionnaireDraftRepository, QuestionnaireDraftHeadRepository questionnaireDraftHeadRepository) {
         this.mapper = mapper;
         this.questionnaireDraftRepository = questionnaireDraftRepository;
+        this.questionnaireDraftHeadRepository = questionnaireDraftHeadRepository;
     }
 
-    private QuestionnaireDraftEntity requireDraft(UUID id) {
-        return questionnaireDraftRepository.findById(id)
+    private void requireDraft(UUID id) {
+        questionnaireDraftRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Questionnaire draft not found: " + id));
     }
 
+    @Transactional
     public UUID createDraft(QuestionnaireRequestDto dto) {
-        QuestionnaireDraftEntity questionnaireDraft = questionnaireDraftRepository.save(mapper.toEntity(dto));
-        return questionnaireDraft.getId();
+        UUID uuid = UUID.randomUUID();
+        questionnaireDraftHeadRepository.ensureHeadExists(uuid);
+        long version = questionnaireDraftHeadRepository.allocateNextVersion(uuid);
+        QuestionnaireDraftEntity questionnaireDraft = questionnaireDraftRepository.save(mapper.toEntity(dto, uuid, version));
+        return uuid;
     }
 
-    public QuestionnaireRequestDto getDraft(UUID id) throws EntityNotFoundException {
-        QuestionnaireDraftEntity questionnaireDraft = requireDraft(id);
-        return mapper.toDto(questionnaireDraft);
+    @Transactional()
+    public QuestionnaireRequestDto getLatestDraft(UUID draftId) {
+        QuestionnaireDraftEntity e = questionnaireDraftRepository.findFirstByKeyDraftIdOrderByKeyVersionDesc(draftId)
+                .orElseThrow(() -> new EntityNotFoundException("Questionnaire draft not found: " + draftId));
+        return mapper.toDto(e);
     }
 
-    public UUID updateDraft(UUID id, QuestionnaireRequestDto dto) throws EntityNotFoundException {
-        QuestionnaireDraftEntity existing = requireDraft(id);
-        mapper.updateEntity(existing, dto);
-        questionnaireDraftRepository.save(existing);
-        return existing.getId();
+    @Transactional
+    public QuestionnaireRequestDto getDraft(UUID draftId, long version) {
+        QuestionnaireDraftEntity e = questionnaireDraftRepository.findByKeyDraftIdAndKeyVersion(draftId, version)
+                .orElseThrow(() -> new EntityNotFoundException("Questionnaire draft not found: " + draftId + " and version: " + version));
+        return mapper.toDto(e);
+
     }
+
+    @Transactional
+    public long addDraftVersion(UUID draftId, QuestionnaireRequestDto dto) {
+        requireDraft(draftId);
+        questionnaireDraftHeadRepository.ensureHeadExists(draftId);
+
+        long version = questionnaireDraftHeadRepository.allocateNextVersion(draftId);
+        questionnaireDraftRepository.save(mapper.toEntity(dto, draftId, version));
+        return version;
+    }
+
 }
